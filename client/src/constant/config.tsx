e4ad5f12f99a1865a230262c4c67327d20b5eb69
Yolo-10 <2684921675@qq.com>
2023-02-08 17:42:44 +0800

    add oauth README.md + scope

--- client/src/constant/config.tsx
+++ client/src/constant/config.tsx
@@ -10,5 +10,6 @@ export const MENU_MY = [
 
 export const GITHUB_CONFIG = {
     clientID : '55280d745fd8bb0a098e',
-    redirectUri : 'http://localhost:8030/oauth'
+    redirectUri : 'http://localhost:8030/oauth',
+    scope : "user:email"
 }
--- client/src/constant/urls.tsx
+++ client/src/constant/urls.tsx
@@ -1,4 +1,6 @@
 import { GITHUB_CONFIG } from "./config";
 
 export const API_GITHUB = `https://github.com/login/oauth/authorize?
-client_id=${GITHUB_CONFIG.clientID}&redirect_uri=${GITHUB_CONFIG.redirectUri}`
+client_id=${GITHUB_CONFIG.clientID}
+&scope=${GITHUB_CONFIG.scope}
+&redirect_uri=${GITHUB_CONFIG.redirectUri}`
