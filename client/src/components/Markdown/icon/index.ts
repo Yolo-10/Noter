183b7765ab334e3728df89554d323330b1fc38c4
Yolo-10 <2684921675@qq.com>
2023-02-27 01:01:55 +0800

    add: init Markdown

---
+++ client/src/components/Markdown/icon/index.ts
@@ -0,0 +1,50 @@
+import bold from "./bold.svg";
+import codeblock from "./codeblock.svg";
+import img from "./img.svg";
+import italic from "./italic.svg";
+import link from "./link.svg";
+import ordList from "./ordList.svg";
+import quote from "./quote.svg";
+import reCall from "./reCall.svg";
+import save from "./save.svg";
+import split from "./split.svg";
+import strThrough from "./strThrough.svg";
+import title from "./title.svg";
+import unOrdList from "./unOrdList.svg";
+import todo from "./todo.svg";
+
+export const svgLfIcons = [
+  bold,
+  italic,
+  title,
+  strThrough,
+  ordList,
+  unOrdList,
+  todo,
+  quote,
+  codeblock,
+  img,
+  link,
+  reCall,
+  save,
+];
+
+export const svgLfText = [
+  "加粗",
+  "斜体",
+  "标题",
+  "删除线",
+  "有序",
+  "无序",
+  "代办",
+  "引用",
+  "代码块",
+  "图片",
+  "链接",
+  "回退",
+  "保存",
+];
+
+export const svgRtIcons = [split];
+
+export const svgRtText = ["分屏"];
--- client/src/components/Markdown/index.tsx
+++ client/src/components/Markdown/index.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { svgLfIcons, svgLfText, svgRtIcons, svgRtText } from './icon/index'
+import { marked } from 'marked'
+import './index.scss'
 
 const Markdown: React.FC = () => {
-  return <div>111</div>
+  const [prev, setPreV] = useState('');
+  const editBox = useRef<HTMLTextAreaElement | null>(null)
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    breaks: true,
+    smartLists: true,
+    smartypants: true,   
+  }); 
+
+  const click = () => {
+    console.log(111)
+  }
+  
+  const doInsPre = () => {
+    if(editBox?.current?.value) setPreV(marked(editBox.current.value))
+  }
+
+  return <div className='md-editor'>
+    <div className="md-editor-toolbar">
+      <ul>
+        {svgLfText.map((item, i) =>
+          <li key={`svgLfIcon-${i}`} className="m-icon" onClick={click}>
+            <img src={svgLfIcons[i]} />
+            <span>{item}</span>
+          </li>)}
+      </ul>
+      <ul>
+        {svgRtText.map((item, i) =>
+            <li key={`svgRtIcon-${i}`} className="m-icon">
+              <img src={svgRtIcons[i]} />
+              <span>{item}</span>
+            </li>)}
+      </ul>
+    </div>
+    <div className="md-editor-content">
+      <textarea className="md-editor-input"
+        ref={editBox}
+        onKeyUp={doInsPre}
+        defaultValue=""/>
+      <div className="md-editor-preview" dangerouslySetInnerHTML={{__html:prev}}></div>
+    </div>
+  </div>
 }
 
 export default Markdown
