7754f3b20b6965f3ff88f28b233329ba836a7ac3
Yolo-10 <2684921675@qq.com>
2023-03-01 17:14:46 +0800

    add:上方左侧图标点击事件完成

--- client/src/components/Markdown/icon/index.ts
+++ client/src/components/Markdown/icon/index.ts
@@ -1,50 +1,121 @@
 import bold from "./bold.svg";
 import codeblock from "./codeblock.svg";
-import img from "./img.svg";
 import italic from "./italic.svg";
 import link from "./link.svg";
 import ordList from "./ordList.svg";
 import quote from "./quote.svg";
-import reCall from "./reCall.svg";
-import save from "./save.svg";
-import split from "./split.svg";
 import strThrough from "./strThrough.svg";
 import title from "./title.svg";
 import unOrdList from "./unOrdList.svg";
 import todo from "./todo.svg";
 
-export const svgLfIcons = [
-  bold,
-  italic,
-  title,
-  strThrough,
-  ordList,
-  unOrdList,
-  todo,
-  quote,
-  codeblock,
-  img,
-  link,
-  reCall,
-  save,
+// import img from "./img.svg";
+// import reCall from "./reCall.svg";
+// import save from "./save.svg";
+// import split from "./split.svg";
+
+// export const svgLfIcons = [
+//   bold,
+//   italic,
+//   title,
+//   strThrough,
+//   ordList,
+//   unOrdList,
+//   todo,
+//   quote,
+//   codeblock,
+//   img,
+//   link,
+//   reCall,
+//   save,
+// ];
+// export const svgLfText = [
+//   "加粗",
+//   "斜体",
+//   "标题",
+//   "删除线",
+//   "有序",
+//   "无序",
+//   "代办",
+//   "引用",
+//   "代码块",
+//   "图片",
+//   "链接",
+//   "回退",
+//   "保存",
+// ];
+// export const svgRtIcons = [split];
+// export const svgRtText = ["分屏"];
+
+export const svgS = [
+  {
+    svgIcon: bold,
+    svgIntro: "加粗",
+    lfChar: "**",
+    rtChar: "**",
+    str: "加粗样式",
+  },
+  {
+    svgIcon: italic,
+    svgIntro: "斜体",
+    lfChar: "***",
+    rtChar: "***",
+    str: "斜体样式",
+  },
+  {
+    svgIcon: strThrough,
+    svgIntro: "删除线",
+    lfChar: "~~",
+    rtChar: "~~",
+    str: "删除线样式",
+  },
+  {
+    svgIcon: title,
+    svgIntro: "标题",
+    lfChar: "## ",
+    rtChar: "",
+    str: "标题",
+  },
+  {
+    svgIcon: ordList,
+    svgIntro: "有序",
+    lfChar: "\n1. ",
+    rtChar: "",
+    str: "List item",
+  },
+  {
+    svgIcon: unOrdList,
+    svgIntro: "无序",
+    lfChar: "\n- ",
+    rtChar: "",
+    str: "List item",
+  },
+  {
+    svgIcon: todo,
+    svgIntro: "代办",
+    lfChar: "\n- [ ] ",
+    rtChar: "",
+    str: "List item",
+  },
+  {
+    svgIcon: quote,
+    svgIntro: "引用",
+    lfChar: "> ",
+    rtChar: "\n",
+    str: "这里是引用",
+  },
+  {
+    svgIcon: codeblock,
+    svgIntro: "代码块",
+    lfChar: "`",
+    rtChar: "`",
+    str: "在这里插入代码片",
+  },
+  {
+    svgIcon: link,
+    svgIntro: "链接",
+    lfChar: "[",
+    rtChar: "](添加链接url)",
+    str: "添加链接描述",
+  },
 ];
-
-export const svgLfText = [
-  "加粗",
-  "斜体",
-  "标题",
-  "删除线",
-  "有序",
-  "无序",
-  "代办",
-  "引用",
-  "代码块",
-  "图片",
-  "链接",
-  "回退",
-  "保存",
-];
-
-export const svgRtIcons = [split];
-
-export const svgRtText = ["分屏"];
--- client/src/components/Markdown/index.tsx
+++ client/src/components/Markdown/index.tsx
@@ -1,12 +1,21 @@
-import React, { useRef, useState } from 'react'
-import { svgLfIcons, svgLfText, svgRtIcons, svgRtText } from './icon/index'
+import React, { useEffect, useRef, useState } from 'react'
+import { svgS } from './icon/index'
 import { marked } from 'marked'
 import hljs from 'highlight.js'
 import './github.css'
 import './index.scss'
 
+interface svgType {
+  svgIcon: any,
+  svgIntro: string,
+  lfChar: string,
+  rtChar: string,
+  str:string,
+}
+
 const Markdown: React.FC = () => {
-  let scrolling = 0, timer: any = null;
+  let scrolling = 0, timer1: any = null,timer2: any = null;
+  const [inpV,setInpV] = useState('')
   const [prev, setPreV] = useState('')
   const refInput = useRef<HTMLTextAreaElement | null>(null)
   const refPre = useRef<HTMLDivElement | null>(null)
@@ -22,8 +31,31 @@ const Markdown: React.FC = () => {
     highlight: code => hljs.highlightAuto(code).value
   }); 
 
-  const click = () => {
-    console.log(111)
+  const doClick = (item: svgType) => {
+    // 获取光标位置
+    let selSt = refInput.current?.selectionStart || 0
+    let selEn = refInput.current?.selectionEnd || 0
+    // textarea新文字
+    let str = inpV.slice(0, selSt) + item.lfChar
+      + (selSt === selEn ? item.str : inpV.slice(selSt, selEn))
+      + item.rtChar + inpV.slice(selEn)
+    // 新光标需要体谅的位置下标
+    let newSelSt = selSt + item.lfChar.length;
+    let newSelEd = selEn + item.lfChar.length + (selSt === selEn ? item.str.length : 0)
+    
+    /**
+    setInpV是异步操作，setInpV导致inpV改变，组件重新render了一次
+    如果不使用setTimeout开启异步操作,程序会先setSelectionRange后再render
+    使得光标选择的消失了，就没有效果了
+    而setTimeout开启一个定时器,就要及时去除定时器,否则当定时器过多时会引起内存泄露
+    */
+    if(timer2) clearTimeout(timer2)
+    timer2 = setTimeout(() => {
+      refInput.current?.focus()
+      refInput.current?.setSelectionRange(newSelSt, newSelEd)
+      clearTimeout(timer2)
+    }, 50)
+    setInpV(str)
   }
 
   const doSynScroll = (e: any, obj: number) => {
@@ -38,32 +70,37 @@ const Markdown: React.FC = () => {
   const driveScroll = (scale:number,obj:any) => {
     let sH = (refPre.current?.scrollHeight || 0) * scale;
     obj.current?.scrollTo({ top: sH })
-    if(timer) clearTimeout(timer)
-    timer = setTimeout(() => {
+    if(timer1) clearTimeout(timer1)
+    timer1 = setTimeout(() => {
       scrolling = 0
-      clearTimeout(timer)
+      clearTimeout(timer1)
     },200)
   }
   
-  const doInsPre = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-     setPreV(marked((e.target as HTMLTextAreaElement).value))
+  const doInsPre = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInpV(e.target.value)
   }
+
+  useEffect(() => {
+    setPreV(marked(inpV))
+  }, [inpV])
 
   return <div className='md-editor'>
     <div className="md-editor-toolbar">
       <ul>
-        {svgLfText.map((item, i) =>
-          <li key={`svgLfIcon-${i}`} className="m-icon" onClick={click}>
-            <img src={svgLfIcons[i]} />
-            <span>{item}</span>
+        {svgS.map((item, i) =>
+            <li key={i} className="m-icon"
+              onClick={()=>doClick(item)}>
+             <img src={item.svgIcon} />
+              <span>{ item.svgIntro}</span>
           </li>)}
       </ul>
       <ul>
-        {svgRtText.map((item, i) =>
+        {/* {svgRtText.map((item, i) =>
             <li key={`svgRtIcon-${i}`} className="m-icon">
               <img src={svgRtIcons[i]} />
               <span>{item}</span>
-            </li>)}
+            </li>)} */}
       </ul>
     </div>
     <div className="md-editor-content">
@@ -71,9 +108,9 @@ const Markdown: React.FC = () => {
         ref={refInput}
         className="md-editor-input"
         spellCheck="false"
-        onKeyUp={e => doInsPre(e)}
-        onScroll={e =>doSynScroll(e,1)}
-        defaultValue="" />
+        value={inpV}
+        onChange = {e => doInsPre(e)}
+        onScroll={e =>doSynScroll(e,1)} />
       <div
         ref={refPre}
         className="md-editor-preview"
